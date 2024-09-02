1706d5581d2112ecd1a74ba50497783665376e0c
Mihir Sahu <0xmihirsahu@gmail.com>
2024-09-02 18:47:24 +0530

    added kinto sdk with kyc button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,30 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Wrapper } from "./Wrapper";
+"use client"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const Header = () => {
-  return (
-    <header className="py-8 border-b mb-10">
-      <Wrapper>
-        <div className="flex items-center justify-between">
-          <h1 className="text-lg md:text-xl font-bold">
-            Solidity Next.js Starter
-          </h1>
-          <ConnectButton
-            showBalance={false}
-            accountStatus="address"
-            label="Connect"
-          />
-        </div>
-      </Wrapper>
-    </header>
-  );
-};
+  const pathname = usePathname();
 
-export { Header };
+  return (
+    <header className="py-4 w-full px-24 mb-0 flex items-center justify-between z-10 bg-gradient-to-b from-white via-white to-transparent">
+      <div className="flex justify-center items-center gap-6">
+        <Link href="/" className="text-3xl md:text-xl font-bold text-emerald-500">
+          Deel
+        </Link>
+        <Link href="/kyc" className={` ${pathname === "/kyc" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}>KYC</Link>
+        <Link href="/jobs" className={` ${pathname === "/jobs" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}>Jobs</Link>
+      </div>
+      <div>
+        <ConnectButton
+          showBalance={false}
+          accountStatus="address"
+          label="Connect"
+        />
+      </div>
+    </header>
+  )
+}
+
+export { Header }
