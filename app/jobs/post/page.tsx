c175ac5ee03910292095eac8807cf350aa72751b
Paul <mierlo.paul@hotmail.nl>
2024-09-03 21:07:56 +0200

    added job post page

---
+++ app/jobs/post/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Post() {
+  return <div className="flex flex-col items-center justify-center">Post</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Wrapper } from "@/components/Wrapper";
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
+"use client"
+
+import { Wrapper } from "@/components/Wrapper"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
 const Home = () => {
+  const router = useRouter()
+
   return (
-    <main >
+    <main>
       <div className="flex flex-col items-center justify-center">
         <div className="flex items-center justify-center mb-10">
           <h1 className="text-4xl font-bold text-center text-gray-900">
@@ -11,12 +17,21 @@ const Home = () => {
           </h1>
         </div>
         <div className="flex flex-row justify-center items-center gap-6">
-          <Button className="rounded-lg bg-emerald-900 hover:bg-emerald-700"><Link href={"/"}>I am looking to hire</Link></Button>
-          <Button className="rounded-lg bg-emerald-900  hover:bg-emerald-700"><Link href={"/"}>I want to offer Pro services</Link></Button>
+          <Button
+            onClick={() => {
+              router.push("/jobs/post")
+            }}
+            className="rounded-lg bg-emerald-900 hover:bg-emerald-700"
+          >
+            <Link href={"/"}>I am looking to hire</Link>
+          </Button>
+          <Button className="rounded-lg bg-emerald-900  hover:bg-emerald-700">
+            <Link href={"/"}>I want to offer Pro services</Link>
+          </Button>
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
