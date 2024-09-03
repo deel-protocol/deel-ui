4d296e758c630afa727c29f59b773633af9e62ea
Paul <mierlo.paul@hotmail.nl>
2024-09-03 21:19:37 +0200

    add job posting UI

--- app/jobs/post/page.tsx
+++ app/jobs/post/page.tsx
@@ -1,5 +1,82 @@
 import React from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Post() {
-  return <div className="flex flex-col items-center justify-center">Post</div>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {" "}
+      <h1 className="text-4xl font-bold text-center text-gray-900">
+        Post your Job
+      </h1>
+      <Card className="w-[350px] mt-32">
+        <CardHeader>
+          <CardTitle>Create Job Listing</CardTitle>
+          <CardDescription>
+            Post your job listing with one simple click
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Category</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Webdesign</SelectItem>
+                    <SelectItem value="sveltekit">Code auditing</SelectItem>
+                    <SelectItem value="astro">
+                      Smart contract development
+                    </SelectItem>
+                    <SelectItem value="nuxt">Landing page creation</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Framework</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Next.js</SelectItem>
+                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                    <SelectItem value="astro">Astro</SelectItem>
+                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Post</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
