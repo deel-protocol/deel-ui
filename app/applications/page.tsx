be307aa17da9553f0a50cacc22d5191d50942972
Paul <mierlo.paul@hotmail.nl>
2024-09-04 23:50:24 +0200

    job application list

---
+++ app/applications/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+
+export default function Applications() {
+  const [applicantview, setApplicantView] = useState<boolean>(false)
+  const jobListings = [
+    {
+      title: "Web desinger",
+      description: "A web designer to create our landing page",
+      price: "$100",
+    },
+    // Add more job posts as needed
+  ]
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div>
+        {applicantview ? (
+          <div></div>
+        ) : (
+          <>
+            <p>Your outstanding job applications</p>
+            {jobListings.map((job, index) => (
+              <div
+                key={index}
+                className="mt-10 mb-4 bg-white rounded-lg p-4 shadow w-[500px]"
+              >
+                <h2 className="text-xl font-bold mb-2">{job.title}</h2>
+                <p className="mb-2">{job.description}</p>
+                <p className="mb-2">Price: {job.price}</p>
+                <Button
+                  onClick={() => setApplicantView(true)}
+                  className="rounded-lg bg-emerald-900 hover:bg-emerald-700"
+                >
+                  View all applicants
+                </Button>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,35 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 const Header = () => {
-  const pathname = usePathname();
+  const pathname = usePathname()
 
   return (
     <header className="py-4 w-full px-24 mb-0 flex items-center justify-between z-10 bg-gradient-to-b from-white via-white to-transparent">
       <div className="flex justify-center items-center gap-6">
-        <Link href="/" className="text-3xl md:text-xl font-bold text-emerald-500">
+        <Link
+          href="/"
+          className="text-3xl md:text-xl font-bold text-emerald-500"
+        >
           Deel
         </Link>
-        <Link href="/kyc" className={` ${pathname === "/kyc" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}>KYC</Link>
-        <Link href="/jobs" className={` ${pathname === "/jobs" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}>Jobs</Link>
+        <Link
+          href="/kyc"
+          className={` ${pathname === "/kyc" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+        >
+          KYC
+        </Link>
+        <Link
+          href="/jobs"
+          className={` ${pathname === "/jobs" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+        >
+          Jobs
+        </Link>
+        <Link
+          href="/applications"
+          className={` ${pathname === "/applications" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+        >
+          Applications
+        </Link>
       </div>
       <div>
         <ConnectButton
