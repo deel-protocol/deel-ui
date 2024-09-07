c9a19b21acbd33875767042e178859347202941b
Paul <mierlo.paul@hotmail.nl>
2024-09-07 21:26:05 +0200

    added new routes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,38 +9,39 @@ import { AuroraBackground } from "@/components/aurora-background"
 const Home = () => {
   const router = useRouter()
   const { user, setUser } = useUserContext()
+  console.log("current logged in user is:", user)
 
   return (
     <AuroraBackground>
-    <main>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center mb-10">
-          <h1 className="text-4xl font-bold text-center text-gray-900">
-            Welcome to Deel
-          </h1>
+      <main>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center mb-10">
+            <h1 className="text-4xl font-bold text-center text-gray-900">
+              Welcome to Deel
+            </h1>
+          </div>
+          <div className="flex flex-row justify-center items-center gap-6">
+            <Button
+              onClick={() => {
+                router.push("/jobs/post/new")
+                setUser("poster")
+              }}
+              className="rounded-lg bg-emerald-900 hover:bg-emerald-700"
+            >
+              <Link href={"/"}>Create a job posting</Link>
+            </Button>
+            <Button
+              onClick={() => {
+                router.push("/jobs")
+                setUser("applier")
+              }}
+              className="rounded-lg bg-emerald-900  hover:bg-emerald-700"
+            >
+              <Link href={"/"}>Browse all Jobs</Link>
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-6">
-          <Button
-            onClick={() => {
-              router.push("/jobs/post/new")
-              setUser("poster")
-            }}
-            className="rounded-lg bg-emerald-900 hover:bg-emerald-700"
-          >
-            <Link href={"/"}>Create a job posting</Link>
-          </Button>
-          <Button
-            onClick={() => {
-              router.push("/jobs")
-              setUser("applier")
-            }}
-            className="rounded-lg bg-emerald-900  hover:bg-emerald-700"
-          >
-            <Link href={"/"}>Browse all Jobs</Link>
-          </Button>
-        </div>
-      </div>
-    </main>
+      </main>
     </AuroraBackground>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,12 @@ import { useUserContext } from "@/hooks/useUserContext"
 const Header = () => {
   const pathname = usePathname()
   const { user } = useUserContext()
+  console.log("current user is:", user)
 
   return (
-    <header className={`py-4 w-full px-24 mb-0 flex items-center justify-between z-10 ${pathname=== "/" || pathname === "/home" ? "fixed top-0": ""} bg-gradient-to-b from-white via-white to-transparent`}>
+    <header
+      className={`py-4 w-full px-24 mb-0 flex items-center justify-between z-10 ${pathname === "/" || pathname === "/home" ? "fixed top-0" : ""} bg-gradient-to-b from-white via-white to-transparent`}
+    >
       <div className="flex justify-center items-center gap-6">
         <Link
           href="/"
@@ -20,27 +23,40 @@ const Header = () => {
         <Link
           href="/kyc"
           className={` ${pathname === "/kyc" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
-        >
-        </Link>
-        {user === "poster" || user === "applier" && (<Link
-          href="/jobs"
-          className={` ${pathname === "/jobs" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
-        >
-          Jobs
-        </Link>)}
-        {user === "poster" && (<Link
-          href="/my-listings"
-          className={` ${pathname === "/my-listings" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
-        >
-          My Listings
-        </Link>)}
-        {user === "applier" && (<Link
-          href="/my-applications"
-          className={` ${pathname === "/my-applications" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
-        >
-          My Applications
-        </Link>)}
-
+        ></Link>
+        {user === "poster" ||
+          (user === "applier" && (
+            <Link
+              href="/jobs"
+              className={` ${pathname === "/jobs" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+            >
+              Jobs
+            </Link>
+          ))}
+        {user === "poster" && (
+          <Link
+            href="/my-listings"
+            className={` ${pathname === "/my-listings" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+          >
+            My Listings
+          </Link>
+        )}
+        {user === "applier" && (
+          <Link
+            href="/my-applications"
+            className={` ${pathname === "/my-applications" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+          >
+            My Applications
+          </Link>
+        )}
+        {(user === "applier" || user === "poster") && (
+          <Link
+            href="/profile"
+            className={` ${pathname === "/profile" ? "text-blue-600 font-bold" : "text-gray-700 font-semibold"}`}
+          >
+            My Profile
+          </Link>
+        )}
       </div>
       <div>
         <Connect />
